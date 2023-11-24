223ddf3011fb327b4f987fd39275366eed52f95e
Md-Mansor <mansor.me777@ gmail.com>
2023-11-24 10:18:43 +0600

    connected server with mongoDB

--- index.js
+++ index.js
@@ -11,6 +11,40 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express());
 
+// user : final-project 
+// pass : Q0pLr2VsR0z9oQY7
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://final-project:Q0pLr2VsR0z9oQY7@cluster0.xl4aigt.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('tech-project is running')

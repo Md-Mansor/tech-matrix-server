63f5fee28097fdefd9146839701dcdeae69504e6
Md-Mansor <mansor.me777@ gmail.com>
2023-11-29 01:58:22 +0600

    no change

--- index.js
+++ index.js
@@ -59,18 +59,18 @@ async function run() {
 
         app.delete("/users/:id", async (req, res) => {
             const id = req.params.id;
-            console.log(id);
             const query = { _id: new ObjectId(id) }
             const result = await userCollection.deleteOne(query);
             res.send(result);
         })
-
+        // make user moderator or admin
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updateDoc = {
                 $set: {
-                    role: "admin"
+                    role: "moderator",
+                    role: "admin",
                 }
             }
             const result = await userCollection.updateOne(filter, updateDoc);
